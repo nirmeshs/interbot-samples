1054ce3ea4a25199882382186961c7c7d6d9adf3
Nirmesh <nirmesh@wofro.work>
2017-06-06 20:46:52 +0530

    Basic http bot

---
+++ basic-interbot/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const name = 'node-hello-world';
+const port = '5000';
+
+const app = new http.Server();
+
+app.on('request', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello World 123');
+  res.end('\n');
+});
+
+app.listen(port, () => {
+  console.log(`${name} is listening on port ${port}`);
+});
